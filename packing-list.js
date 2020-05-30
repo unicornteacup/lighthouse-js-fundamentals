cdc19c9809a769a6d0d424a6c5e29f803aa9f8e9
Caroline <carolinejepps@gmail.com>
2020-05-30 18:57:12 +0000

    Array list

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,9 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
